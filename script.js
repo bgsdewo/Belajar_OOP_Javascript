a27c9f2b0494ee3772aed6d6050076b0d2e52b9e
unknown <bgsdewoo20@gmail.com>
2024-09-25 08:17:47 +0700

    topic Membuat Object Dengan Factory Function

--- script.js
+++ script.js
@@ -19,3 +19,37 @@
 //   console.log("Method dinonaktifkan!");
 // };
 // kelasDasar.pop();
+
+//topic Membuat Object Dengan Factory Function
+// const hex = (r, g, b) => {
+//   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// };
+// const rgb = (r, g, b) => {
+//   return `rgb(${r},${g},${b})`;
+// };
+// console.log(hex(106, 22, 90));
+// console.log(rgb(106, 22, 90));
+
+function convertColor(r, g, b) {
+  const color = {};
+
+  color.r = r;
+  color.g = g;
+  color.b = b;
+
+  color.rgb = function () {
+    const { r, g, b } = this;
+    return `rgb(${r}, ${g}, ${b})`;
+  };
+
+  color.hex = function () {
+    const { r, g, b } = this;
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  };
+
+  return color;
+}
+
+const color = convertColor(123, 123, 123);
+console.log(color.rgb()); // Output: rgb(123, 123, 123)
+console.log(color.hex()); // Output: #7b7b7b

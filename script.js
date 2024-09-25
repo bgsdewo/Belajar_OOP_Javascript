cc08e8708e20c9ea0516f4198d5023d49e023bce
unknown <bgsdewoo20@gmail.com>
2024-09-25 07:44:43 +0700

    Perkenalan Tentang Prototype Di Javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+//topic Perkenalan Tentang Prototype Di Javascript
+// const arr = [1, 2];
+// arr.hello = () => {
+//   console.log("hello world");
+// };
+// arr.hello();
+// //mengcostum method pada object array utama
+// Array.prototype.greeting = () => {
+//   console.log("hello world");
+// };
+// //mewarisi method greeting pada variabel dibawah ini
+// const kelasDasar = [1, 2, 3];
+// const kelasMenengah = [4, 5, 6];
+// //memakai methodnya
+// kelasDasar.greeting();
+// kelasMenengah.greeting();
+// //Mengoverride method yang sudah ada dalam array
+// Array.prototype.pop = () => {
+//   console.log("Method dinonaktifkan!");
+// };
+// kelasDasar.pop();

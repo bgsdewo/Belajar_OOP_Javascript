7a07d76ab9f8a695e1c80ed7027bf1f7a6e27012
unknown <bgsdewoo20@gmail.com>
2024-09-25 09:25:50 +0700

    Membuat Object Dengan Constructor Function

--- script.js
+++ script.js
@@ -21,6 +21,7 @@
 // kelasDasar.pop();
 
 //topic Membuat Object Dengan Factory Function
+
 // const hex = (r, g, b) => {
 //   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 // };
@@ -30,26 +31,74 @@
 // console.log(hex(106, 22, 90));
 // console.log(rgb(106, 22, 90));
 
-function convertColor(r, g, b) {
-  const color = {};
+// function convertColor(r, g, b) {
+//   const color = {};
 
-  color.r = r;
-  color.g = g;
-  color.b = b;
+//   color.r = r;
+//   color.g = g;
+//   color.b = b;
 
-  color.rgb = function () {
-    const { r, g, b } = this;
-    return `rgb(${r}, ${g}, ${b})`;
-  };
+//   color.rgb = function () {
+//     const { r, g, b } = this;
+//     return `rgb(${r}, ${g}, ${b})`;
+//   };
 
-  color.hex = function () {
-    const { r, g, b } = this;
-    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-  };
+//   color.hex = function () {
+//     const { r, g, b } = this;
+//     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//   };
 
-  return color;
-}
+//   return color;
+// }
 
-const color = convertColor(123, 123, 123);
-console.log(color.rgb()); // Output: rgb(123, 123, 123)
-console.log(color.hex()); // Output: #7b7b7b
+// const color = convertColor(123, 123, 123);
+// console.log(color.rgb()); // Output: rgb(123, 123, 123)
+// console.log(color.hex()); // Output: #7b7b7b
+
+//topic Membuat Object Dengan Constructor Function
+// function convertColor(r, g, b) {
+//   const color = {};
+
+//   color.r = r;
+//   color.g = g;
+//   color.b = b;
+
+//   color.rgb = function () {
+//     const { r, g, b } = this;
+//     return `rgb(${r}, ${g}, ${b})`;
+//   };
+
+//   color.hex = function () {
+//     const { r, g, b } = this;
+//     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//   };
+
+//   return color;
+// }
+
+// function Color(r, g, b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
+// }
+// Color.prototype.rgb = function () {
+//   const { r, g, b } = this;
+//   return `rgb(${r}, ${g}, ${b})`;
+// };
+
+// Color.prototype.hex = function () {
+//   const { r, g, b } = this;
+//   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// };
+// // console.log(new Color(11, 199, 4).rgb());
+// // console.log(new Color(11, 199, 4).hex());
+// // const objColor = new Color(12, 200, 89);
+// // console.log(objColor.hex());
+// // console.log(objColor);
+// // console.log(convertColor(123, 123, 123));
+
+// Color.prototype.rgba = function (a = 1.0) {
+//   const { r, g, b } = this;
+//   return `rgba(${r}, ${g}, ${b},${a})`;
+// };
+// document.body.style.backgroundColor = new Color(19, 200, 30).rgba(0.2);
